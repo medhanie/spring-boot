0a1114e81f523b4440fc6895bd03e50feea602cc
medhanie <medhanie@outlook.com>
2016-03-09 00:14:28 -0500

    formatting code

--- src/main/java/com/medhanie/github/spboot/model/Person.java
+++ src/main/java/com/medhanie/github/spboot/model/Person.java
@@ -9,37 +9,45 @@ public class Person {
 	private String middleName;
 	private String lastName;
 	private java.sql.Date dateOfBirth;
-	
+
 	public BigInteger getId() {
 		return id;
 	}
+
 	public void setId(BigInteger id) {
 		this.id = id;
 	}
+
 	public String getFirsName() {
 		return firsName;
 	}
+
 	public void setFirsName(String firsName) {
 		this.firsName = firsName;
 	}
+
 	public String getMiddleName() {
 		return middleName;
 	}
+
 	public void setMiddleName(String middleName) {
 		this.middleName = middleName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public java.sql.Date getDateOfBirth() {
 		return dateOfBirth;
 	}
+
 	public void setDateOfBirth(java.sql.Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
-	
-	
+
 }
--- src/main/java/com/medhanie/github/spboot/web/api/PersonController.java
+++ src/main/java/com/medhanie/github/spboot/web/api/PersonController.java
@@ -16,12 +16,12 @@ import com.medhanie.github.spboot.model.Person;
 
 @RestController
 public class PersonController {
-	
+
 	private static BigInteger nextId;
 	private static Map<BigInteger, Person> personsMap;
-	
-	private static Person save(Person person){
-		if(personsMap == null){
+
+	private static Person save(Person person) {
+		if (personsMap == null) {
 			personsMap = new HashMap<BigInteger, Person>();
 			nextId = BigInteger.ONE;
 		}
@@ -30,29 +30,26 @@ public class PersonController {
 		nextId = nextId.add(BigInteger.ONE);
 		return person;
 	}
-	
+
 	static {
 		Person p1 = new Person();
 		p1.setFirsName("Medhanie");
 		p1.setLastName("Mihreteab");
 		p1.setDateOfBirth(java.sql.Date.valueOf("1990-07-04"));
-		
+
 		Person p2 = new Person();
 		p2.setFirsName("Sessen");
 		p2.setMiddleName("Berhane");
 		p2.setLastName("Beraki");
-		
+
 		save(p1);
 		save(p2);
 	}
 
-	@RequestMapping(
-			value = "/api/persons",
-			method = RequestMethod.GET,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Collection<Person>> getPersons(){
-		Collection<Person> persons = personsMap.values();
+	@RequestMapping(value = "/api/persons", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<Person>> getPersons() {
 		
+		Collection<Person> persons = personsMap.values();
 		return new ResponseEntity<Collection<Person>>(persons, HttpStatus.OK);
 	}
 }
